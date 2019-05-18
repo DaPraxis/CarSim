db88c2771ae4d1aee266f11e92b89436c9a8c743
Rikin <dolllyamar@gmail.com>
2019-05-17 22:34:34 -0400

    data.js: cleanup - changed to follow es6 class style, changed getters/setters to normal functions

--- data.js
+++ data.js
@@ -1,87 +1,100 @@
-var data = {
-    userId,
-    settings,
-    dateTime,
-    time_stemp: [],
-    speed_report: [],
-    NPCs: [],
-    collisions: [],
-    input_aggregate,
-    interactions,
-    init(userId,todo_style, off_road){
+class Data {
+    constructor(userId, todo_style, off_road) {
+        // userId: string
         this.userId = userId;
+        // settings: object for config options
+        this.settings = {};
+        // todo style: string representing whether objectives are in todo list,
+        //             in single objective format, or etc
         this.settings.todo_style = todo_style;
+        // off_road: bool representing whether or not the user can go off road
         this.off_road = off_road;
+        // dateTime: datetime at start of game
         this.dateTime = Date.now();
-    },
+        this.time_stamps = [];
+        // NPCs: list of NPCs met during the game
+        this.NPCs = [];
+        this.speed_report = [];
+        // collisions: list of objects collided with?
+        this.collisions = [];
+        // input_aggregate and interactions: aggregate data, to be computed at
+        //                                   end of game
+        this.input_aggregate = {};
+        this.interactions = {};
+    }
 
-    get get_time_stemp() {
-        return this.time_stemp;
-    },
-    set set_time_stemp(start_time, end_time, start_coor, end_coor, opperation) {
-        var obj = new Object();
-        obj.start_time  =start_time;
-        obj.end_time = end_time;
-        obj.start_coor = start_coor;
-        obj.end_coor = end_coor;
-        obj.opperation = opperation;
-        this.time_stemp = [...time_stemp, obj]
-    },
+    get_time_stamps() {
+        return this.time_stamps;
+    }
 
-    get get_speed_report(){
+    log_time_stamps(start_time, end_time, start_coor, end_coor, operation) {
+        let obj = {
+            start_time: start_time,
+            end_time: end_time,
+            start_coor: start_coor,
+            end_coor: end_coor,
+            operation: operation
+        }
+        this.time_stamps = [...this.time_stamps, obj]
+    }
+
+    get_speed_report() {
         return this.speed_report;
-    },
+    }
 
-    set set_speed_report(start_time, end_time, speed_mode){
-        var obj  =new Object();
-        obj.start_time = start_time;
-        obj.end_time = end_time;
-        obj.speed_mode = speed_mode;
+    log_speed_report(start_time, end_time, speed_mode) {
+        let obj = {
+            start_time: start_time,
+            end_time: end_time,
+            speed_mode: speed_mode
+        }
         this.speed_report = [...speed_report, obj];
-    },
+    }
 
-    get get_NPCs(){
+    get_NPCs_list() {
         return this.NPCs;
-    },
+    }
 
-    set set_NPCs(start_time, NPC, complete){
-        var obj =  new Object();
-        obj.start_time = start_time;
-        obj.NPC = NPC;
-        obj.complete = complete;
+    log_NPC(start_time, NPC, complete) {
+        var obj = {
+            start_time: start_time,
+            NPC: NPC,
+            complete: complete
+        }
         this.NPCs = [...NPCs, obj];
-    },
+    }
 
-    get get_collisions(){
+    get_collisions() {
         return this.collisions;
-    },
+    }
 
-    set set_Collisions(start_time, end_time, object){
-        var obj = new Object();
-        obj.start_time = start_time;
-        obj.end_time = end_time;
-        obj.object = object;
+    log_collisions(start_time, end_time, object) {
+        let obj = {
+            start_time: start_time,
+            end_time: end_time,
+            object: object
+        }
         this.collisions = [...collisions, obj];
-    },
+    }
 
-// process data will process all data in raw and generate aggregated data, can be overwrotten any time
-    process(todo_status){
-        this.input_aggregate.num_inputs = this.time_stemp.length;
+    // process data will process all data in raw and generate aggregated data, can be overwritten any time
+    process(todo_status) {
+        this.input_aggregate.num_inputs = this.time_stamps.length;
         this.input_aggregate.max_time_input = 0;
-        for (var temp of this.time_stemp){
+        for (let temp of this.time_stamps) {
             this.input_aggregate.max_time_input += temp.end_time - temp.start_time;
         }
         this.input_aggregate.collisions = this.collisions.length;
 
         this.input_aggregate.collision_time = 0;
-        for (temp of this.collisions){
+        for (temp of this.collisions) {
             this.input_aggregate.collision_time += temp.end_time - temp.start_time;
         }
         this.interactions.all_interactions = this.NPCs.length;
         this.interactions.completed_interactions = 0;
-        for (temp of this.NPCs){
-            if (temp.complete){
-                this.interactions.completed_interactions +=1;
+        for (temp of this.NPCs) {
+            if (temp.complete) {
+                this.interactions.completed_interactions += 1;
             }
         }
         this.todo_finished = todo_status;
